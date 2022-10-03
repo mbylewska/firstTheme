2745fad5cc5bcb6cedfe0ea012528a86f7315c60
Marta Bylewska <martabylewska@gmail.com>
2022-10-03 09:59:16 +0200

    working on standard page

--- themes/mb-theme/page.php
+++ themes/mb-theme/page.php
@@ -4,6 +4,29 @@
 
 <section class=" slider_section position-relative" style="background-image: url(<?php the_post_thumbnail_url(); ?>)">
 
+    <?php $hero = get_field('hero'); ?>
+    <div class="container">
+        <div class="detail-box">
+            <h1><?php echo $hero['title']; ?><br>
+                <?php echo $hero['title_second']; ?>
+
+            </h1>
+            <div class="row">
+                <div class="col-lg-6 col-md-8">
+                    <p><?php echo $hero['text']; ?>
+                    </p>
+                </div>
+            </div>
+            <div class="btn-box">
+                <a href="<?php echo $hero['link']; ?>" class="btn1">
+                    <?php echo $hero['link_text']; ?>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+
 
 </section>
 <!-- end slider section -->
