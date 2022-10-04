f75e27517d6f151ebedd03017ab6e294c46e383b
Marta Bylewska <martabylewska@gmail.com>
2022-10-04 13:08:10 +0200

    footer - working with icons

--- themes/mb-theme/footer.php
+++ themes/mb-theme/footer.php
@@ -6,17 +6,19 @@
         </h5>
         <div class="social_box">
             <a href="#">
-                <img src="<?php bloginfo('template_directory'); ?>/images/fb.png" alt="">
+                <img src="<?php bloginfo('template_directory'); ?>/images/facebook50.png" alt="">
             </a>
 
             <a href="#">
-                <img src="<?php bloginfo('template_directory'); ?>/images/linkedin.png" alt="">
+                <img src="<?php bloginfo('template_directory'); ?>/images/linkedin50.png" alt="">
             </a>
             <a href="#">
-                <img src="<?php bloginfo('template_directory'); ?>/images/instagram.png" alt="">
+                <img src="<?php bloginfo('template_directory'); ?>/images/instagram50.png" alt="">
             </a>
 
         </div>
+
+        <p>All icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
 
         <p>
             &copy; <span id="displayYear"></span> All Rights Reserved By
